ccdbdfa016b09fb687f09064ed9de6e6fadc103d
Nikola Sirmov <sirmov0213@gmail.com>
2022-04-07 19:39:02 +0300

    Login input validation

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,5 +1,58 @@
-import { loginTemplate } from '../views/loginView.js';
+import { loginTemplate } from '../views/loginView';
+import { createSubmitHandler } from '../utils/decorators';
+
+const allowedData = ['username', 'email', 'password'];
+const emailRegex = /\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b/;
+const numbersRegex = /\d/;
+const specialCharactersRegex = /[^A-z\s\d][\\\^]?/;
 
 export function loginController(ctx, next) {
-    ctx.render(loginTemplate());
+    ctx.render(loginTemplate(createSubmitHandler(ctx, loginSubmit, allowedData)));
+}
+
+async function loginSubmit(ctx, data, event) {
+    let validation = {};
+    allowedData.forEach((d) => (validation[d] = { isValid: true, message: '' }));
+
+    // Username validation
+    if (data.username.length < 6) {
+        validation.username.isValid = false;
+        validation.username.message = 'Username should be at least 6 characters long.';
+    } else {
+        validation.username.isValid = true;
+        validation.username.message = 'Username is available.';
+    }
+
+    // Email validation
+    if (!emailRegex.test(data.email)) {
+        validation.email.isValid = false;
+        validation.email.message = 'Email is incorrect.';
+    } else {
+        validation.email.isValid = true;
+        validation.email.message = 'Email is correct.';
+    }
+
+    // Password validation
+    if (data.password.length < 8) {
+        validation.password.isValid = false;
+        validation.password.message = 'Password should be at least 8 characters long. ';
+    }
+    if (!numbersRegex.test(data.password)) {
+        validation.password.isValid = false;
+        validation.password.message += 'Password should contain numbers. ';
+    }
+    if (!specialCharactersRegex.test(data.password)) {
+        validation.password.isValid = false;
+        validation.password.message += 'Password should contain special characters.';
+    }
+    if (validation.password.isValid === true) {
+        validation.password.message = 'Password is valid.';
+    }
+
+    if (Object.entries(validation).some(([k, v]) => v.isValid === false)) {
+        ctx.render(loginTemplate(createSubmitHandler(ctx, loginSubmit, allowedData), validation));
+    } else {
+        ctx.render(loginTemplate(createSubmitHandler(ctx, loginSubmit, allowedData), validation));
+        // Todo add login logic
+    }
 }
--- src/views/loginView.js
+++ src/views/loginView.js
@@ -1,38 +1,62 @@
-import { html } from 'lit-html';
+import { html, nothing } from 'lit-html';
 
 export const loginTemplate = (loginSubmit, validation) =>
     html`<form @submit=${loginSubmit} class="container">
         <div class="field">
             <label class="label is-medium" for="username">Username</label>
             <div class="control has-icons-left">
-                <input class="input" type="text" id="username" name="username" placeholder="peter16" />
+                <input
+                    class="input ${validation ? (validation.username.isValid ? 'is-success' : 'is-danger') : ''}"
+                    type="text"
+                    id="username"
+                    name="username"
+                    placeholder="peter16"
+                />
                 <span class="icon is-small is-left">
                     <i class="fa-solid fa-user"></i>
                 </span>
             </div>
-            <p class="help is-success">This username is available</p>
+            <p class="help ${validation ? (validation.username.isValid ? 'is-success' : 'is-danger') : ''}">
+                ${validation ? validation.username.message : nothing}
+            </p>
         </div>
 
         <div class="field">
             <label class="label is-medium" for="email">Email</label>
             <div class="control has-icons-left">
-                <input class="input" type="email" id="email" name="email" placeholder="peter@gmail.com" />
+                <input
+                    class="input ${validation ? (validation.email.isValid ? 'is-success' : 'is-danger') : ''}"
+                    type="email"
+                    id="email"
+                    name="email"
+                    placeholder="peter@gmail.com"
+                />
                 <span class="icon is-small is-left">
                     <i class="fa-solid fa-envelope"></i>
                 </span>
             </div>
-            <p class="help is-success">This username is available</p>
+            <p class="help ${validation ? (validation.email.isValid ? 'is-success' : 'is-danger') : ''}">
+                ${validation ? validation.email.message : nothing}
+            </p>
         </div>
 
         <div class="field">
             <label class="label is-medium" for="password">Password</label>
             <div class="control has-icons-left">
-                <input class="input" type="password" id="password" name="password" placeholder="********" />
+                <input
+                    class="input ${validation ? (validation.password.isValid ? 'is-success' : 'is-danger') : ''}"
+                    type="password"
+                    id="password"
+                    name="password"
+                    placeholder="********"
+                />
                 <span class="icon is-small is-left">
                     <i class="fa-solid fa-lock"></i>
                 </span>
             </div>
-            <p class="help is-success">This username is available</p>
+            <p class="help ${validation ? (validation.password.isValid ? 'is-success' : 'is-danger') : ''}">
+                ${validation ? validation.password.message : nothing}
+            </p>
         </div>
 
         <div class="field">
