b39fc170ec45596fd453235580eb46c6e621c151
Nikola Sirmov <sirmov0213@gmail.com>
2022-04-07 18:52:28 +0300

    Decaration util module

---
+++ utils/decorators.js
@@ -0,0 +1,24 @@
+export function createSubmitHandler(ctx, handler, allowedData) {
+    return function (event) {
+        event.preventDefault();
+
+        const formData = new FormData(event.target);
+        const formEntries = [...formData.entries()];
+        let data;
+
+        if (allowedData !== undefined) {
+            data = formEntries.reduce((a, [k, v]) => (allowedData.includes(k) ? Object.assign(a, { [k]: v }) : a), {});
+        } else {
+            data = Object.fromEntries(formData);
+        }
+
+        handler(ctx, data, event);
+    };
+}
+
+export function createEventHandler(ctx, handler) {
+    return function (event) {
+        event.preventDefault();
+        handler(ctx, event);
+    };
+}
