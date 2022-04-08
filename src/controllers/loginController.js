13979ac0292c5cfc9b85f45b6aab67300c440390
Nikola Sirmov <sirmov0213@gmail.com>
2022-04-08 15:58:52 +0300

    Tweaked login view

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,7 +1,8 @@
 import { loginTemplate } from '../views/loginView';
 import { createSubmitHandler } from '../utils/decorators';
+import { login } from '../services/authenticationService';
 
-const allowedData = ['username', 'email', 'password'];
+const allowedData = ['email', 'password'];
 const emailRegex = /\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b/;
 const numbersRegex = /\d/;
 const specialCharactersRegex = /[^A-z\s\d][\\\^]?/;
@@ -14,22 +15,10 @@ async function loginSubmit(ctx, data, event) {
     let validation = {};
     allowedData.forEach((d) => (validation[d] = { isValid: true, message: '' }));
 
-    // Username validation
-    if (data.username.length < 6) {
-        validation.username.isValid = false;
-        validation.username.message = 'Username should be at least 6 characters long.';
-    } else {
-        validation.username.isValid = true;
-        validation.username.message = 'Username is available.';
-    }
-
     // Email validation
     if (!emailRegex.test(data.email)) {
         validation.email.isValid = false;
         validation.email.message = 'Email is incorrect.';
-    } else {
-        validation.email.isValid = true;
-        validation.email.message = 'Email is correct.';
     }
 
     // Password validation
@@ -45,14 +34,12 @@ async function loginSubmit(ctx, data, event) {
         validation.password.isValid = false;
         validation.password.message += 'Password should contain special characters.';
     }
-    if (validation.password.isValid === true) {
-        validation.password.message = 'Password is valid.';
-    }
 
     if (Object.entries(validation).some(([k, v]) => v.isValid === false)) {
         ctx.render(loginTemplate(createSubmitHandler(ctx, loginSubmit, allowedData), validation));
     } else {
-        ctx.render(loginTemplate(createSubmitHandler(ctx, loginSubmit, allowedData), validation));
-        // Todo add login logic
+        await login(ctx.auth, data.email, data.password);
+        event.target.reset();
+        ctx.page.redirect('/');
     }
 }
--- src/views/loginView.js
+++ src/views/loginView.js
@@ -3,29 +3,10 @@ import { html, nothing } from 'lit-html';
 export const loginTemplate = (loginSubmit, validation) =>
     html`<form @submit=${loginSubmit} class="container">
         <div class="field">
-            <label class="label is-medium" for="username">Username</label>
-            <div class="control has-icons-left">
-                <input
-                    class="input ${validation ? (validation.username.isValid ? 'is-success' : 'is-danger') : ''}"
-                    type="text"
-                    id="username"
-                    name="username"
-                    placeholder="peter16"
-                />
-                <span class="icon is-small is-left">
-                    <i class="fa-solid fa-user"></i>
-                </span>
-            </div>
-            <p class="help ${validation ? (validation.username.isValid ? 'is-success' : 'is-danger') : ''}">
-                ${validation ? validation.username.message : nothing}
-            </p>
-        </div>
-
-        <div class="field">
             <label class="label is-medium" for="email">Email</label>
             <div class="control has-icons-left">
                 <input
-                    class="input ${validation ? (validation.email.isValid ? 'is-success' : 'is-danger') : ''}"
+                    class="input ${validation ? (!validation.email.isValid ? 'is-danger' : '') : ''}"
                     type="email"
                     id="email"
                     name="email"
@@ -35,7 +16,7 @@ export const loginTemplate = (loginSubmit, validation) =>
                     <i class="fa-solid fa-envelope"></i>
                 </span>
             </div>
-            <p class="help ${validation ? (validation.email.isValid ? 'is-success' : 'is-danger') : ''}">
+            <p class="help ${validation ? (!validation.email.isValid ? 'is-danger' : '') : ''}">
                 ${validation ? validation.email.message : nothing}
             </p>
         </div>
@@ -44,7 +25,7 @@ export const loginTemplate = (loginSubmit, validation) =>
             <label class="label is-medium" for="password">Password</label>
             <div class="control has-icons-left">
                 <input
-                    class="input ${validation ? (validation.password.isValid ? 'is-success' : 'is-danger') : ''}"
+                    class="input ${validation ? (!validation.password.isValid ? 'is-danger' : '') : ''}"
                     type="password"
                     id="password"
                     name="password"
@@ -54,14 +35,18 @@ export const loginTemplate = (loginSubmit, validation) =>
                     <i class="fa-solid fa-lock"></i>
                 </span>
             </div>
-            <p class="help ${validation ? (validation.password.isValid ? 'is-success' : 'is-danger') : ''}">
+            <p class="help ${validation ? (!validation.password.isValid ? 'is-danger' : '') : ''}">
                 ${validation ? validation.password.message : nothing}
             </p>
         </div>
 
         <div class="field">
+            <p class="content">Dont't have an account? Sign up <a href="/sign-up">here</a>.</p>
+        </div>
+
+        <div class="field">
             <div class="control">
-                <button class="button is-primary" type="submit">Submit</button>
+                <button class="button is-primary" type="submit">Log in</button>
             </div>
         </div>
     </form>`;
