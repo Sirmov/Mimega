81e2e9b385046c09a4b9582b3854714283a163d2
Nikola Sirmov <sirmov0213@gmail.com>
2022-04-12 17:02:54 +0300

    Update selector function

--- src/utils/dom.js
+++ src/utils/dom.js
@@ -5,3 +5,11 @@ export const spinner = () => html`<div class="spinner">
     <span class="spinner-inner-2"></span>
     <span class="spinner-inner-3"></span>
 </div>`;
+
+export function updateSelector(element, selector) {
+    if (element == undefined || element.parentElement == null) {
+        element = document.querySelector(selector);
+    }
+
+    return element;
+}
