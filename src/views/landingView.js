399fb10419497cca1e2ca35aa06e7827ad26a8b5
Nikola Sirmov <sirmov0213@gmail.com>
2022-04-15 21:08:02 +0300

    Added subtitle

--- src/views/landingView.js
+++ src/views/landingView.js
@@ -4,5 +4,7 @@ export const landingTemplate = () =>
     html`<section class="hero">
         <div class="hero-body">
             <h1 class="title is-1">Mimega</h1>
+            <h2 class="subtitle is-3">The online meme platform</h2>
         </div>
     </section>`;
+ 
