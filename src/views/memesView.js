dbdda3e14ac5ad3d1ef06a9063e75a8b39b26ddb
Nikola Sirmov <sirmov0213@gmail.com>
2022-04-15 21:07:30 +0300

    Made meme cards mobile responsive

--- src/views/memesView.js
+++ src/views/memesView.js
@@ -4,9 +4,7 @@ import { until } from 'lit-html/directives/until.js';
 import { spinner } from '../utils/dom';
 
 export const memesTemplate = (memesPromise) =>
-    html`<ul class="container">
-        ${until(memesPromise, spinner())}
-    </ul>`;
+    html`<div class="columns is-multiline is-variable is-6">${until(memesPromise, spinner())}</div>`;
 
 export const memeCardsTemplate = (memes, onDelete) =>
     html`${memes.length > 0
@@ -18,27 +16,30 @@ export const memeCardsTemplate = (memes, onDelete) =>
         : html`<h1>No memes sorry :(</h1>`}`;
 
 const memeCardTemplate = (meme, onDelete) =>
-    html`<div class="card meme-card">
-        <header class="card-header">
-            <h1 class="card-header-title title">${meme.title}</h1>
-        </header>
-        <div class="card-image">
-            <figure class="image is-5by4">
-                <img src=${meme.imageUrl} alt="Meme image" />
-            </figure>
-        </div>
-        <div class="card-content">
-            <div class="content">
-                <p class="title is-6">${meme.author}</p>
-                <time>${meme.updatedAt.toDate().toLocaleDateString()}</time>
+    html`<div class="column is-half">
+        <div class="card meme-card">
+            <header class="card-header">
+                <h1 class="card-header-title title is-size-5-desktop is-size-6-mobile">${meme.title}</h1>
+            </header>
+            <div class="card-image">
+                <figure class="image is-5by4">
+                    <img src=${meme.imageUrl} alt="Meme image" />
+                </figure>
             </div>
+            <div class="card-content">
+                <div class="content">
+                    <p class="title is-6">${meme.author}</p>
+                    <time>${meme.updatedAt.toDate().toLocaleDateString()}</time>
+                </div>
+            </div>
+            <footer class="card-footer">
+                ${meme.isOwner
+                    ? html`<a href="/edit-meme/${meme.id}" class="card-footer-item">Edit</a>
+                          <a href="javascript:void(0)" data-id=${meme.id} class="card-footer-item" @click=${onDelete}
+                              >Delete</a
+                          >`
+                    : nothing}
+                <a href="#" class="card-footer-item">Save</a>
+            </footer>
         </div>
-        <footer class="card-footer">
-            ${meme.isOwner
-                ? html`<a href="/edit-meme/${meme.id}" class="card-footer-item">Edit</a>
-                      <a href="javascript:void(0)" data-id=${meme.id} class="card-footer-item" @click=${onDelete}
-                          >Delete</a>`
-                : nothing}
-            <a href="#" class="card-footer-item">Save</a>
-        </footer>
     </div>`;
