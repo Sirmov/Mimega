9482377aed46141778972555fb235346238c1a17
Nikola Sirmov <sirmov0213@gmail.com>
2022-04-19 18:57:15 +0300

    Social media links in footer

--- src/views/skeleton/footerView.js
+++ src/views/skeleton/footerView.js
@@ -3,7 +3,19 @@ import { html } from 'lit-html';
 export const footerTemplate = () =>
     html`<div class="content has-text-centered">
         <p>
-            <strong>Mimega</strong> by <a href="https://github.com/Sirmov">Nikola Sirmov</a>. The source code is
-            licensed <a href="http://opensource.org/licenses/mit-license.php">MIT</a> &copy; 2022.
+            <strong>Mimega</strong> by Nikola Sirmov. The source code is licensed
+            <a href="https://github.com/Sirmov/Mimega/blob/main/LICENSE">MIT</a> &copy; 2022.
+        </p>
+        <p>
+            ${linkTemplate('fa-brands fa-github', 'https://github.com/Sirmov', 'Github')}
+            ${linkTemplate('fa-brands fa-linkedin', 'https://www.linkedin.com/in/sirmov/', 'Linkedin')}
+            ${linkTemplate(
+                'fa-brands fa-facebook',
+                'https://www.facebook.com/people/Nikola-Sirmov/100000683407598/',
+                'Facebook'
+            )}
+            ${linkTemplate('fa-brands fa-instagram', 'https://www.instagram.com/sirmov04/', 'Instagram')}
         </p>
     </div>`;
+
+const linkTemplate = (icon, link, title) => html`<a href=${link} class="px-1"><i class="${icon} px-1"></i>${title}</a>`;
