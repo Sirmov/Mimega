7fce5d265cc35ca752d516f7c5235985fded8cda
Nikola Sirmov <sirmov0213@gmail.com>
2022-05-06 10:34:17 +0300

    Renamed like and unlike actions

--- src/actions/likeMemeAction.js
+++ src/actions/likeMemeAction.js
@@ -1,6 +1,10 @@
+import { renderMemeFooter } from '../controllers/memeController';
 import { createLike } from '../services/likesService';
+import { updateMemeOnLike } from '../views/memeView';
 
 export async function likeMemeAction(ctx, event) {
     const memeId = event.target.dataset.id;
     await createLike(ctx.db, ctx.auth, memeId);
+    updateMemeOnLike(renderMemeFooter(ctx), true);
+    event.target.remove();
 }
--- src/actions/unlikeMemeAction.js
+++ src/actions/unlikeMemeAction.js
@@ -1,6 +1,10 @@
+import { renderMemeFooter } from '../controllers/memeController';
 import { deleteUserLike } from '../services/likesService';
+import { updateMemeOnLike } from '../views/memeView';
 
 export async function unlikeMemeAction(ctx, event) {
     const memeId = event.target.dataset.id;
     await deleteUserLike(ctx.db, ctx.auth, memeId);
+    updateMemeOnLike(renderMemeFooter(ctx), false);
+    event.target.remove();
 }
