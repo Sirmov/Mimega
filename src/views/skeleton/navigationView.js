98c92c7eec42a44986eda2a6e5f221349c685e25
Nikola Sirmov <sirmov0213@gmail.com>
2022-05-07 20:03:38 +0300

    Removed href

--- src/views/skeleton/navigationView.js
+++ src/views/skeleton/navigationView.js
@@ -33,7 +33,7 @@ export const navigationTemplate = (displayName, onLogout) =>
                               <a class="button is-primary" href="/create-meme">Upload meme</a>
                           </div>
                           <div class="navbar-item">
-                              <a class="button is-light" href="javascript:void(0)" @click=${onLogout}>Logout</a>
+                              <a class="button is-light" @click=${onLogout}>Logout</a>
                           </div>`
                     : html`<div class="navbar-item">
                           <div class="buttons">
