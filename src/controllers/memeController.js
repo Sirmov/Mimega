7ff43d07a1fe96e6089b7e0a7bd47beb59fc30e4
Nikola Sirmov <sirmov0213@gmail.com>
2022-05-07 18:07:10 +0300

    Comments view

--- src/controllers/memeController.js
+++ src/controllers/memeController.js
@@ -2,18 +2,20 @@ import { deleteMemeAction } from '../actions/deleteMemeAction';
 import { likeMemeAction } from '../actions/likeMemeAction';
 import { unlikeMemeAction } from '../actions/unlikeMemeAction';
 import { getUserUid } from '../services/authenticationService';
+import { readAllComments } from '../services/commentsService';
 import { readMeme } from '../services/memesService';
 import { createEventHandler } from '../utils/decorators';
-import { memeCardTemplate, memeFooterTemplate, memeTemplate } from '../views/memeView';
+import { commentsTemplate, memeCardTemplate, memeFooterTemplate, memeTemplate } from '../views/memeView';
 
-let onDelete, onLike, onUnlike;
+let onDelete, onLike, onUnlike, onSubmit;
 
 export function memeController(ctx, next) {
     onDelete = createEventHandler(ctx, deleteMemeAction);
     onLike = createEventHandler(ctx, likeMemeAction);
     onUnlike = createEventHandler(ctx, unlikeMemeAction);
+    onSubmit = createEventHandler(ctx, onCommentSubmit);
 
-    ctx.render(memeTemplate(renderMemeCard(ctx)));
+    ctx.render(memeTemplate(renderMemeCard(ctx), renderComments(ctx)));
 }
 
 async function renderMemeCard(ctx) {
@@ -21,9 +23,18 @@ async function renderMemeCard(ctx) {
     return memeCardTemplate(meme, onDelete, onLike, onUnlike);
 }
 
+async function renderComments(ctx) {
+    let comments = await readAllComments(ctx.db, ctx.params.id);
+    return commentsTemplate(comments, onSubmit);
+}
+
 export async function renderMemeFooter(ctx) {
     let meme = await fetchMeme(ctx);
     return memeFooterTemplate(meme, onDelete, onLike, onUnlike);
+}
+
+async function onCommentSubmit(ctx, data, event) {
+
 }
 
 async function fetchMeme(ctx) {
--- src/views/memeView.js
+++ src/views/memeView.js
@@ -7,19 +7,12 @@ import { getDate, spinner } from '../utils/dom';
 const cardFooter = createRef();
 const likesRef = createRef();
 
-export const memeTemplate = (memePromise) => html`<div class="columns">
-    ${until(memePromise, spinner())}
-    <div class="column is-half ">
-        <div class="box">
-            <h1 class="title is-size-3">Comments</h1>
-            <p class="content is-size-5">Under development. 👾</p>
-        </div>
-    </div>
-</div>`;
+export const memeTemplate = (memePromise, commentsPromise) =>
+    html`<div class="columns">${until(memePromise, spinner())} ${until(commentsPromise, spinner())}</div>`;
 
 export const memeCardTemplate = (meme, onDelete, onLike, onUnlike) =>
     html` <div class="column is-half">
-        <div class="card meme-card">
+        <section class="card meme-card">
             <header class="card-header">
                 <h1 class="card-header-title title is-size-5-desktop is-size-6-touch">${meme.title}</h1>
             </header>
@@ -43,7 +36,7 @@ export const memeCardTemplate = (meme, onDelete, onLike, onUnlike) =>
             <footer ${ref(cardFooter)} class="card-footer">
                 ${memeFooterTemplate(meme, onDelete, onLike, onUnlike)}
             </footer>
-        </div>
+        </section>
     </div>`;
 
 export function updateMemeOnLike(memePromise, isLike) {
@@ -69,11 +62,81 @@ export const memeFooterTemplate = (meme, onDelete, onLike, onUnlike) =>
               `/edit-meme/${meme.id}`
           )}
           ${cardFooterItemTemplate(meme, 'Delete', 'danger', 'fa-solid fa-trash', onDelete)}`
-        : meme.isLogged 
+        : meme.isLogged
         ? meme.isLiked
-        ? html`${cardFooterItemTemplate(meme, 'Unlike', 'danger', 'fa-solid fa-heart', onUnlike)}`
-        : html`${cardFooterItemTemplate(meme, 'Like', 'danger', 'fa-solid fa-heart', onLike)}`
+            ? html`${cardFooterItemTemplate(meme, 'Unlike', 'danger', 'fa-solid fa-heart', onUnlike)}`
+            : html`${cardFooterItemTemplate(meme, 'Like', 'danger', 'fa-solid fa-heart', onLike)}`
         : html`${cardFooterItemTemplate(meme, 'You have to be logged in to like memes.', 'link')}`}`;
+
+export const commentsTemplate = (comments, onSubmit, validation) =>
+    html`<div class="column is-half">
+        <section class="box">
+            <h1 class="title is-size-3">Comments</h1>
+            <div class="container comments mb-5">
+                ${repeat(
+                    comments,
+                    (comment) => comment.id,
+                    (comment, index) => commentTemplate(comment)
+                )}
+            </div>
+            <div class="container comments-form">${commentFormTemplate(onSubmit, validation)}</div>
+        </section>
+    </div>`;
+
+export const commentFormTemplate = (onSubmit, validation) =>
+    html`<form @submit=${onSubmit} class="container">
+        <label class="label is-medium" for="comment">Post a comment</label>
+        <div class="field is-grouped">
+            <div class="control has-icons-left is-expanded">
+                <input
+                    class="input ${validation ? (validation.comment.isValid ? 'is-success' : 'is-danger') : ''}"
+                    type="text"
+                    id="comment"
+                    name="comment"
+                    placeholder="Cool meme broski!"
+                />
+                <span class="icon is-small is-left">
+                    <i class="fa-solid fa-comment"></i>
+                </span>
+            </div>
+            <div class="control">
+                <button class="button is-primary mr-5" type="submit">Submit comment</button>
+            </div>
+            <p class="help ${validation ? (validation.comment.isValid ? 'is-success' : 'is-danger') : ''}">
+                ${validation ? validation.comment.message : nothing}
+            </p>
+        </div>
+    </form>`;
+
+const commentTemplate = (comment) =>
+    html`<div class="box">
+        <div class="content">
+            <p><strong>${comment.author}</strong></p>
+            <p>${comment.comment}</p>
+        </div>
+        <nav class="level is-mobile">
+            <div class="level-left">
+                <a class="level-item" aria-label="reply">
+                    <span class="icon is-small">
+                        <i class="fas fa-reply" aria-hidden="true"></i>
+                    </span>
+                </a>
+                <a class="level-item" aria-label="retweet">
+                    <span class="icon is-small">
+                        <i class="fas fa-retweet" aria-hidden="true"></i>
+                    </span>
+                </a>
+                <a class="level-item" aria-label="like">
+                    <span class="icon is-small">
+                        <i class="fas fa-heart" aria-hidden="true"></i>
+                    </span>
+                </a>
+            </div>
+            <div class="level-right">
+                <p>Last updated: ${getDate(comment.updatedAt)}</p>
+            </div>
+        </nav>
+    </div>`;
 
 const cardFooterItemTemplate = (meme, text, color, icon, eventHandler = null, link = 'javascript:void(0)') =>
     html`<a
