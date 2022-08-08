804acf116a7d067b743c3de867f5c32a34e9cb03
Nikola Sirmov <sirmov0213@gmail.com>
2022-08-08 21:55:58 +0300

    Is rendered utility function

--- src/utils/dom.js
+++ src/utils/dom.js
@@ -1,7 +1,6 @@
 import { html } from 'lit-html';
 
-export const spinner = () => html`
-<div class="columns is-centered is-mobile">
+export const spinner = () => html` <div class="columns is-centered is-mobile">
     <div class="column is-narrow">
         <div id="spinner"></div>
     </div>
@@ -16,7 +15,7 @@ export function updateSelector(element, selector) {
 }
 
 export function simulatePromise(delay) {
-    return new Promise(function(resolve) {
+    return new Promise(function (resolve) {
         setTimeout(resolve, delay);
     });
 }
@@ -30,4 +29,28 @@ export function getDate(timestamp) {
 
     date = dd + '/' + mm + '/' + yyyy;
     return date;
+}
+
+// Mutation observer
+
+let observerContainer;
+const observerConfig = { attributes: false, childList: true, characterData: false, subtree: true };
+
+export function isRendered(elementSelector, afterRenderCallback) {
+    observerContainer = updateSelector(observerContainer, 'main');
+    let observer = new MutationObserver(observerCallback(elementSelector, afterRenderCallback));
+    observer.observe(observerContainer, observerConfig);
+}
+
+function observerCallback(elementSelector, afterRenderCallback) {
+    return function (mutationList, observer) {
+        for (const mutation of mutationList) {
+            if (mutation.type === 'childList') {
+                if (observerContainer.querySelector(elementSelector)) {
+                    afterRenderCallback();
+                    observer.disconnect();
+                }
+            }
+        }
+    };
 }
